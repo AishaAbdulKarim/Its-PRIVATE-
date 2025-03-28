81d3a605b6cba2eb609ef7d7e7816beebf013411
aishat aminu <aishat.aminu@Student.griffith.ie>
2025-03-28 01:26:33 +0000

    Added Egg.java with basic falling logic

--- SD_Game_Project/src/game/Egg.java
+++ SD_Game_Project/src/game/Egg.java
@@ -1,4 +1,31 @@
 package game;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
 public class Egg {
+    private int x;
+    private int y;
+    private int speed = 2;
+    private Image eggImage;
+
+    public Egg(int x, int y, String imageName) {
+        this.x = x;
+        this.y = y;
+        eggImage = new ImageIcon("images/" + imageName).getImage();
+    }
+
+    public void update() {
+        y += speed;
+    }
+
+    public void draw(Graphics2D graphics, JPanel panel) {
+        graphics.drawImage(eggImage, x, y, 40, 50, panel);
+    }
+
+    public int getY() {
+        return y;
+    }
 }
