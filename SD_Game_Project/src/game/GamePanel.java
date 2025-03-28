b42933ccb992ca8e31119a567f4c3d064f7b8769
aishat aminu <aishat.aminu@Student.griffith.ie>
2025-03-28 02:07:47 +0000

    Added game loop timer in GamePanel to enable continuous updates for falling egg git add

--- SD_Game_Project/src/game/GamePanel.java
+++ SD_Game_Project/src/game/GamePanel.java
@@ -12,7 +12,13 @@ public class GamePanel extends JPanel implements KeyListener{
 
     public GamePanel(){
         addKeyListener(this);               // Sets up keyboard event listener
+        setFocusable(true);
+        requestFocusInWindow(); // ensures key input works
         game = new GameManager();
+
+        // Game loop - calls update() 60 times per second
+        javax.swing.Timer timer = new javax.swing.Timer(1000 / 60, e -> update());
+        timer.start();
     }
 
     @Override
