9a09610c4e2793d5156a0bbe09788fcb1f795c8e
aishat aminu <aishat.aminu@Student.griffith.ie>
2025-04-01 14:30:04 +0100

    modified the height of the panel

--- SD_Game_Project/src/gameConstants/Constants.java
+++ SD_Game_Project/src/gameConstants/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     // Display Constants
     public final static int REFRESH_RATE = 100;
     public final static int FRAME_WIDTH = 500;
-    public final static int FRAME_HEIGHT = 900;
+    public final static int FRAME_HEIGHT = 700;
     public final static Color SKY_BLUE = new Color(174,227,245);       // Colour for background
 
     // Bucket Constants
