f5683e21edc449c8485a9da548f4da264fb7f818
Aisha Abdul Karim <aisha.abdulkarim@student.griffith.ie>
2025-04-02 11:34:48 +0100

    Initial commit

---
+++ src/game/GameManager.java
@@ -0,0 +1,53 @@
+package game;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+import gameConstants.Constants;
+public class GameManager {
+
+    /*
+     * This class is where all game assets are added and their positons. ie. basket, falling eggs, score tracker etc.
+     * 
+     */
+    
+     /*
+      * Create instance of your class here
+      */
+    private Basket basket;
+
+    /*
+     * Executed by GamePanel
+     */
+    public GameManager(){
+        start();
+    }
+    
+
+    /*
+     * Initialize classes to be rendered here
+     */
+    public void start(){
+        basket = new Basket("Player One", Constants.BASKET_X, Constants.BASKET_Y, Constants.BASKET_WIDTH, Constants.BASKET_HEIGHT, "basket_01.png");
+    }
+
+    /*
+     * Renders images to panel
+     * Implement graphics.drawImage(image, int x position, int y position, int wifth, int height, panel); to render an image to the panel
+     */
+    public void drawSprites(Graphics2D graphics, JPanel panel){
+        //Draw Basket
+        graphics.drawImage(basket.getImage(), basket.getX(), basket.getY(), basket.getWidth(), basket.getHeight(), panel);
+        
+
+    }
+
+    /*
+     * Implement class methods to run each frame here
+     * basket.move();
+     * egg.spawn();
+     * etc.
+     */
+    public void update(){
+        basket.update();
+    }
+
+}
---
+++ src/game/Init.java
@@ -0,0 +1,39 @@
+package game;
+import javax.swing.JFrame;
+
+import gameConstants.Constants;
+
+public class Init {
+public static void main(String[] args) {
+    
+    /*
+     * Initializes frame and adds panel
+     * Implements additional settings for the frame
+     */
+    JFrame frame = new JFrame();
+    GamePanel panel = new GamePanel();
+    frame.add(panel);
+
+    frame.setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT);           // Sets size of frame (window) (ratio of frameSize variable)
+    frame.setVisible(true);                                               // Sets frame as visable
+    frame.requestFocus();                                                   // Frame takes focus for keyboard and mouse
+    panel.requestFocus();                                                   // Panel takes focus for keyboard and mouse
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                   // Program will terminate on exit from frame
+    frame.setLocationRelativeTo(null);                                    // Sets Frame to center of screen
+    frame.setResizable(false);                                    // Prevents resizing frame
+
+    /*
+     * Game Loop
+     * 
+     */
+    long startTime = System.currentTimeMillis();
+    while(true){
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        if(elapsedTime > Constants.REFRESH_RATE){
+            
+            panel.update();
+            startTime = System.currentTimeMillis();                         // This function must remain at the end of this statement
+        }
+    }
+}
+}
