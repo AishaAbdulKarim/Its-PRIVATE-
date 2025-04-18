33196c7afbf29e43afa9d2c86a863056ac58b5d4
aishat aminu <aishat.aminu@Student.griffith.ie>
2025-04-18 17:01:29 +0100

    Added testEggCaughtIncreasesScoreAndRemovesEgg() stub to test egg–basket collision handling and score update

--- src/unit_tests/UnitTests.java
+++ src/unit_tests/UnitTests.java
@@ -63,7 +63,11 @@ public class UnitTests {
 
 	}
 	@Test
-	void testGameOverFlagAfterLivesZero() {//Ensures that the game sets 'game over' to true when lives reach
+	void testGameOverFlagAfterLivesZero() {//Ensures that the game sets 'game over' to true when lives reach 0
+
+	}
+	@Test
+	void testEggCaughtIncreasesScoreAndRemovesEgg() {//Confirms that score increases by 10 and egg is removed after catching it
 
 	}
 	/*
