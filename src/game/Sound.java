986d00f6505bef0213b4ee04ccbd8c37856bb276
Alan <alan.mcgillivray@griffith.student.ie>
2025-04-23 23:01:13 +0100

    Added Sound class constructor implementation

--- src/game/Sound.java
+++ src/game/Sound.java
@@ -1,4 +1,7 @@
 package game;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import javax.sound.sampled.*;
 
 /*
@@ -8,7 +11,36 @@ public class Sound {
     private Clip clip;
 
     public Sound(String fileName){
-
+        // specify the sound to play
+        // (assuming the sound can be played by the audio system)
+        // from a wave File
+        try {
+            File file = new File(fileName);
+            if(file.exists()){
+                AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+                // load the sound into memory (a Clip)
+                clip = AudioSystem.getClip();
+                clip.open();
+            }else{
+                throw new RuntimeException("Sound: file not found: " + fileName);
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Malformed URL: " + e);
+        }
+        catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Unsupported Audio File: " + e);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Input/Output Error: " + e);
+        }
+        catch (LineUnavailableException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
+        }
     }
 
 
